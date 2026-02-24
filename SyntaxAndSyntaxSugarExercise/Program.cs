b02cd3c0f3ee7487d86407267c585e27989aa5bd
Monica Washington <monica.lynnwashington@gmail.com>
2026-02-23 22:59:26 -0700

    Completed Syntax and Syntax Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -3,8 +3,11 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+            var answer = 4;
+            var response = (answer < 9 ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9");
+            Console.WriteLine(response);
+            
         }
     }
 }
